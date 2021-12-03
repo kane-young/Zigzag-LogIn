007423479791ab25e78607cc4026b18aea3aa314
kane-young <lyw2100@naver.com>
2021-12-03 17:43:34 +0900

    feat: 프로젝트 설정 - 폴더 구조 정립 - swiftlint 설정 (cocoaPods)

---
+++ shop-login/Kane-Login/Kane-Login/Sources/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  Kane-Login
+//
+//  Created by 이영우 on 2021/12/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
---
+++ shop-login/Kane-Login/Kane-Login/Sources/View/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Kane-Login
+//
+//  Created by 이영우 on 2021/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
