1aedafaedd2e156ff3e2b6507fa9a2c4c74267ac
kane-young <lyw2100@naver.com>
2021-12-07 13:30:57 +0900

    feat: 약관 동의 Label에 attributedString 적용

--- shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
+++ shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 class LogInViewController: UIViewController {
 
     static let identifier: String = "LogInViewController"
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var autoLogInAgreeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLabels()
     }
 
+    private func configureLabels() {
+        titleLabel.adjustsFontSizeToFitWidth = true
+        subTitleLabel.adjustsFontSizeToFitWidth = true
+        guard let autoLogInAgreeText = autoLogInAgreeLabel.text else { return }
+        let attributedText = NSMutableAttributedString(string: autoLogInAgreeText)
+        attributedText.addAttribute(.foregroundColor, value: UIColor.systemPink,
+                                   range: (autoLogInAgreeText as NSString).range(of: "자동 로그인 약관"))
+        autoLogInAgreeLabel.attributedText = attributedText
+    }
 }
