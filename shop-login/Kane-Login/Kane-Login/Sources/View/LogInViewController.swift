d94620a7bed6c962a19a1c5bd927db59cd78a004
kane-young <lyw2100@naver.com>
2021-12-09 13:35:33 +0900

    feat: 키보드 프레임을 위해 오토레이아웃 변경 적용

--- shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
+++ shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
@@ -23,6 +23,7 @@ class LogInViewController: UIViewController {
     @IBOutlet weak var logInButtonTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var findButtonBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var passwordBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewBottomConstraint: NSLayoutConstraint!
     private lazy var autoLogInTermsButton: UIButton = {
         let autoLogInTermsButton = UIButton()
         autoLogInTermsButton.setTitle("자동 로그인 연결 약관", for: .normal)
@@ -42,6 +43,17 @@ class LogInViewController: UIViewController {
         configureNavigationBar()
         distinguishAutoLogIn()
         configureTextFields()
+        addTapGestureRecognizer()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setUpNotification()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeNotification()
     }
 
     private func configureNavigationBar() {
@@ -136,6 +148,56 @@ class LogInViewController: UIViewController {
 
     @IBAction func editPasswordTextField(_ sender: UITextField) {
         viewModel.textChanged(sender.text, type: .password)
+    }
+
+    // MARK: Method associated Notification
+    private func setUpNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+
+    private func removeNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    // MARK: Set Keyboard associated Method
+    private func addTapGestureRecognizer() {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(moveDownKeyboard))
+        recognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(recognizer)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let endFrameValue = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue) else {
+            return
+        }
+        let endFrame = endFrameValue.cgRectValue
+        scrollViewBottomConstraint.isActive = false
+        scrollViewBottomConstraint = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+                                                                        constant: -endFrame.height)
+        scrollViewBottomConstraint.isActive = true
+        scrollView.contentInset.bottom = 30
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset.bottom = 0
+        scrollViewBottomConstraint.isActive = false
+        scrollViewBottomConstraint = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        scrollViewBottomConstraint.isActive = true
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc private func moveDownKeyboard() {
+        view.endEditing(true)
     }
 
     private func alert() {
