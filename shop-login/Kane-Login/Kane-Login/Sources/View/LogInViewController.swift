a45174f4a7f8a60e4d97aa52d6f8f4a6f9b8be13
kane-young <lyw2100@naver.com>
2021-12-24 17:46:59 +0900

    refactor: keyboardLayoutGuide를 통해서 키보드 프레임에 반응하도록 수정

--- shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
+++ shop-login/Kane-Login/Kane-Login/Sources/View/LogInViewController.swift
@@ -42,17 +42,8 @@ final class LogInViewController: UIViewController {
         configureNavigationBar()
         distinguishAutoLogIn()
         addTapGestureRecognizer()
+        configureScrollViewConstraints()
         viewModelBind()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setUpNotification()
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        removeNotification()
     }
 
     private func viewModelBind() {
@@ -96,6 +87,12 @@ final class LogInViewController: UIViewController {
         let range = (autoLogInAgreeText as NSString).range(of: Style.AutoLogInAgreeLabel.textForAttributed)
         attributedText.addAttribute(.foregroundColor, value: UIColor.systemPink, range: range)
         autoLogInAgreeLabel.attributedText = attributedText
+    }
+
+    private func configureScrollViewConstraints() {
+        scrollViewBottomConstraint.isActive = false
+        scrollViewBottomConstraint = view.keyboardLayoutGuide.topAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor)
+        scrollViewBottomConstraint.isActive = true
     }
 
     private func configureNavigationBar() {
@@ -169,47 +166,10 @@ final class LogInViewController: UIViewController {
         viewModel.textChanged(sender.text, type: .password)
     }
 
-    private func setUpNotification() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
-                                               name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
-                                               name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
-
-    private func removeNotification() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
     private func addTapGestureRecognizer() {
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(moveDownKeyboard))
         recognizer.cancelsTouchesInView = false
         view.addGestureRecognizer(recognizer)
-    }
-
-    @objc private func keyboardWillShow(_ notification: Notification) {
-        guard let userInfo = notification.userInfo,
-              let endFrameValue = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue) else {
-            return
-        }
-        let endFrame = endFrameValue.cgRectValue
-        scrollViewBottomConstraint.isActive = false
-        scrollViewBottomConstraint = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor,
-                                                                        constant: -endFrame.height)
-        scrollViewBottomConstraint.isActive = true
-        UIView.animate(withDuration: 0.3) {
-            self.view.layoutIfNeeded()
-        }
-    }
-
-    @objc private func keyboardWillHide(_ notification: Notification) {
-        let safeArea = view.safeAreaLayoutGuide
-        scrollViewBottomConstraint.isActive = false
-        scrollViewBottomConstraint = scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
-        scrollViewBottomConstraint.isActive = true
-        UIView.animate(withDuration: 0.3) {
-            self.view.layoutIfNeeded()
-        }
     }
 
     @objc private func moveDownKeyboard() {
